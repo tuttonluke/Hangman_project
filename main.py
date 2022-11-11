dbd1f9f41038aa9c1ddf9c0c426d14460d32e314
Luke Tutton <tuttonluke@gmail.com>
2022-11-11 09:03:04 +0000

    Adds comments to main.py file.

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import random
 
 word_list = ['apple', 'grape', 'strawberry', 'mango', 'raspberry']
 print(word_list)
+# choose a word at random from the list of words
 word = random.choice(word_list)
 print(word)
 # %%
+# Assign user input to variable 'guess',
 guess = input('Enter a single letter: ')
+# check the user input is a single alphabetic character.
 if len(guess) == 1 and guess.isalpha():
     print('Good guess!')
 else:
