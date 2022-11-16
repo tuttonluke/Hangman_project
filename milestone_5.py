8d490d27c5949d5b5be6e97fb0f9d28504be1e8a
Luke Tutton <tuttonluke@gmail.com>
2022-11-16 15:52:33 +0000

    Removes some repeated code.

--- milestone_5.py
+++ milestone_5.py
@@ -72,30 +72,6 @@ class Hangman:
                 # return True if letter_guess is in self.word, False otherwise.
                 return self.check_guess(letter_guess) 
 #%%
-word_list = ['apple', 'grape', 'strawberry', 'mango', 'raspberry']
-
-def play_game(word_list: list) -> str:
-    """This function creates an instance of the Hangman class and plays the game.
-
-    Args:
-        word_list (list): list of words from which to play the hangman game.
-
-    Returns:
-        str: Message of winning or losing.
-    """    
-    game = Hangman(word_list)
-    while True:
-        if game.num_lives == 0:
-            return 'You lost!'
-        elif game.num_letters > 0:
-            game.ask_for_input()
-            print(game.word_guessed, '\n')
-        else:
-            return 'Congratulations, you won the game!'
-
-play_game(word_list)
-            
-        
             
 
 
