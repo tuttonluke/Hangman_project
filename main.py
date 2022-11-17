704bf269eb9b2561651b3a9f91aadc74ff2dad50
Luke Tutton <tuttonluke@gmail.com>
2022-11-17 16:12:23 +0000

    Replaces return statement with print function.

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ def play_game(word_list: list) -> str:
 
     Args:
         word_list (list): list of words from which to play the hangman game.
-
-    Returns:
-        str: Message of winning or losing.
     """    
     game = Hangman(word_list)
     while True:
         if game.num_lives == 0:
-            return 'You lost!'
+            print('You lost!')
         elif game.num_letters > 0:
             game.ask_for_input()
             print(game.word_guessed, '\n')
         else:
-            return 'Congratulations, you won the game!'
-
+            print('Congratulations, you won the game!')
+#%%
 play_game(word_list)
+
+
+    
